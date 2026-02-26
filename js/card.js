5c49506044e3e226388d2bd2c523c86466ab6216
VaaVN <ne4valera@mail.ru>
2026-02-26 19:04:22 +0300

    Initial commit

---
+++ js/card.js
@@ -0,0 +1,585 @@
+let cardDatabase = {
+  categories: {
+    major: [
+      {
+        name: "Шут",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Маг",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Верховная Жрица",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Императрица",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Император",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Иерофант",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Влюблённые",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Колесница",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Сила",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Отшельник",
+        pathToImage: "./asset/img/otsh.jpg",
+        description: `
+У самурая нет цели, только тренировки в отшельничестве и резать камень
+
+Если выпала эта карта то надо идти жоско тренироваться мечу или писать на JS, но в отшельничестве
+либо одному в бамбуковом лесу или в тёмной комнате за компьютером
+
+Работа будет тяжела, но она приведёт к большим целям или к блинчику
+
+Отношений не будет, ты один, путь свой ты пройдёшь один и умрёшь в богатстве`,
+        author: "Куренков.В",
+      },
+      {
+        name: "Колесо Фортуны",
+        pathToImage: "./asset/img/fortune.jpg",
+        description: `Любовь: Судьбоносная встеча,
+перемены в текущем союзе
+Финансы: Неожиданный доход,
+везение в деньгах
+Карта дня: Счастливый случай,
+Судьбоносное решение
+Рекомендация: Тебя ждут неожиданные
+изменения, не сопротивляйся им!`,
+        author: "Нечаев.В",
+      },
+      {
+        name: "Справедливость",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Повешенный",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Смерть",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Умеренность",
+        pathToImage: "./asset/img/umer.jpg",
+        description: `Общее значение: сегодня день равновесия: чуть-чуть работы, чуть-чуть учебы, чуть-чуть сна и чуть-чуть отдыха
+
+Любовный расклад: в отношениях все стабильно, если вы одиноки - это отличный день для свидания с самим собой
+
+Ответ на вопрос: все будет в свое время
+
+Совет: постарайся войти в баланс со Вселенной и насладиться гармонией всех сфер жизни`,
+        author: "Новикова.П",
+      },
+      {
+        name: "Дьявол",
+        pathToImage: "./asset/img/devil.jpg",
+        description: `
+Если ты вытянул эту карту, это, возможно, твой знак немного побыть мразью :) без лишних чувств, кроме желания собственной выгоды. Главное не увлекайся... Хотя, это твое дело.
+работа будет плодотворной, но придется идти по головам
+отношения будут, но никакой искренней любви - только холодный расчет`,
+        author: "Тищенко.Г",
+      },
+      {
+        name: "Башня",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Звезда",
+        pathToImage: "./asset/img/star.jpg",
+        description: `Общее:
+Эта карточка даёт тебе преимущество на всё!
+Ты царь и бог! Всё в этом мире подвластно тебе. Ты герой современности. Двери открываются перед тобой. Девушки падают тебе в ноги.
+Если тебе выпала эта карточка, то поздравляем! Ты прошёл эту игру! Ты король вселенной.
+
+Любовь:
+Всё будет пиздато.
+
+Работа: 
+Всё будет пиздато!
+
+Совет:
+Всё будет пиздато!`,
+        author: "Николенко.С",
+      },
+      {
+        name: "Луна",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+      {
+        name: "Солнце",
+        pathToImage: "./asset/img/sun.jpg",
+        description: `Свет побеждает тьму! Сегодня вас ждет день абсолютной ясности, радости и успеха. Все тучи сомнений рассеются, уступая место теплу и оптимизму.
+В делах: Блестящий результат, выигрыш, заключение сделки.
+
+В любви: Гармония, счастье, возможна долгожданная встреча или предложение.
+
+В здоровье: Прекрасное самочувствие и прилив жизненных сил.`,
+        author: "Морунов.П",
+      },
+      {
+        name: "Суд",
+        pathToImage: "./asset/img/sud.jpg",
+        description: `Быстро закрой все дедлайны или получишь 6
+Любовь
+Без предвзятостей
+Аутизм
+Доброкачественный, да и только
+Совет:
+постарайся пропагандировать аутизм`,
+        author: "Смирнов.И",
+      },
+      {
+        name: "Мир",
+        pathToImage: "./asset/img/card.svg",
+        description: "нету",
+        author: "нету",
+      },
+    ],
+    minor: {
+      cups: [
+        {
+          name: "Туз Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Двойка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Тройка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Четвёрка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Пятёрка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Шестёрка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Семёрка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Восьмёрка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Девятка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Десятка Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Паж Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Рыцарь Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Королева Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Король Кубков",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+      ],
+      wands: [
+        {
+          name: "Туз Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Двойка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Тройка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Четвёрка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Пятёрка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Шестёрка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Семёрка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Восьмёрка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Девятка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Десятка Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Паж Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Рыцарь Жезлов",
+          pathToImage: "./asset/img/zhez.jpg",
+          description: `карта решительности,энтузиазма и энергии;
+ты одновременно лучший,великолепный и аутист`,
+          author: "Казазян.А",
+        },
+        {
+          name: "Королева Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Король Жезлов",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+      ],
+      swords: [
+        {
+          name: "Туз Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Двойка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Тройка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Четвёрка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Пятёрка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Шестёрка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Семёрка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Восьмёрка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Девятка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Десятка Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Паж Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Рыцарь Мечей",
+          pathToImage: "",
+          description: "",
+          author: "",
+        },
+        {
+          name: "Королева Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Король Мечей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+      ],
+      pentacles: [
+        {
+          name: "Туз Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Двойка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Тройка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Четвёрка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Пятёрка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Шестёрка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Семёрка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Восьмёрка Пентаклей",
+          pathToImage: "./asset/img/8pent.jpg",
+          description: `ОБЩЕЕ: развитие
+
+ЛЮБОВЬ: база
+
+РАБОТА: пора развиваться, иди на работу, попробуй что-то новое 
+
+СОВЕТ: белая полоса началась, не упусти момент, купи чикенбургер`,
+          author: "Саблина.М",
+        },
+        {
+          name: "Девятка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Десятка Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Паж Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Рыцарь Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Королева Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+        {
+          name: "Король Пентаклей",
+          pathToImage: "./asset/img/card.svg",
+          description: "нету",
+          author: "нету",
+        },
+      ],
+    },
+  },
+};
+document.addEventListener("DOMContentLoaded", () => {
+  function RandomCard() {
+    let but = document.querySelector("[data-js='but']");
+    let img = document.querySelector("[data-js='img']");
+    let title = document.querySelector("[data-js='name']");
+    let desc = document.querySelector("[data-js='disc']");
+    let auth = document.querySelector("[data-js='author']");
+    but.addEventListener("pointerdown", () => {
+      let card;
+      if (Math.floor(Math.random() * 3) == 0) {
+        card = cardDatabase.categories.major[Math.floor(Math.random() * 23)];
+      } else {
+        switch (Math.floor(Math.random() * 4)) {
+          case 0:
+            card =
+              cardDatabase.categories.minor.cups[
+                Math.floor(Math.random() * 14)
+              ];
+            break;
+          case 1:
+            card =
+              cardDatabase.categories.minor.wands[
+                Math.floor(Math.random() * 14)
+              ];
+            break;
+          case 2:
+            card =
+              cardDatabase.categories.minor.swords[
+                Math.floor(Math.random() * 14)
+              ];
+            break;
+          case 4:
+            card =
+              cardDatabase.categories.minor.pentacles[
+                Math.floor(Math.random() * 14)
+              ];
+            break;
+        }
+      }
+      img.src = card.pathToImage;
+      title.textContent = card.name;
+      desc.textContent = card.description;
+      auth.textContent = card.author;
+    });
+  }
+  RandomCard();
+});
